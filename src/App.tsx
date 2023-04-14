93b13ca40bd04e170fb4f88936703095ec099f39
Lars Poetzel <lars.poetzel@gmail.com>
2023-04-14 12:12:08 +0200

    multi select logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import { useState } from "react";
-import Select from "./Select";
+import Select, { SelectOption } from "./Select";
 
 const options = [
   { label: "first", value: 1 },
   { label: "second", value: 2 },
   { label: "third", value: 3 },
-  { label: "forth", value: 4 },
+  { label: "fourth", value: 4 },
   { label: "fifth", value: 5 },
 ];
 
 function App() {
-  const [value, setValue] = useState<typeof options[0] | undefined>(options[0]);
+  const [value1, setValue1] = useState<SelectOption[]>([options[0]]);
+  const [value2, setValue2] = useState<SelectOption | undefined>(options[0]);
 
   return (
-    <div>
-      <Select options={options} value={value} onChange={(o) => setValue(o)} />
-    </div>
+    <>
+      <Select
+        multiple
+        options={options}
+        value={value1}
+        onChange={(o) => setValue1(o)}
+      />
+      <br />
+      <Select options={options} value={value2} onChange={(o) => setValue2(o)} />
+    </>
   );
 }
 
